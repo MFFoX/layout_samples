c6734b7aaa02c42e233185ff9ed24742f182fc3d
mffox <carrie@leaplogic.net>
2020-07-27 14:09:28 -0500

    add home as a drawer item

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:layout_samples/common/navigation-util.dart';
 import 'package:layout_samples/pages/form.dart';
+import 'package:layout_samples/pages/home-page.dart';
 import 'package:layout_samples/pages/non-scrolling-page.dart';
 import 'package:layout_samples/pages/tabbed-list-page.dart';
 
@@ -48,9 +49,11 @@ class AppDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           _createHeader(),
+          _createDrawerItem(icon: Icons.home, text: 'Home',
+              onTap: () => context.navigateTo(HomePage())),
           _createDrawerItem(icon: Icons.aspect_ratio, text: 'Non-Scrolling', 
               onTap: () => context.navigateTo(NonScrollingPage())),
-          _createDrawerItem(icon: Icons.view_list, text: 'Tabbed List', 
+          _createDrawerItem(icon: Icons.view_list, text: 'Tabbed List',
               onTap: () => context.navigateTo(TabbedListPage())),
           _createDrawerItem(icon: Icons.keyboard, text: 'Form',
               onTap: () => context.navigateTo(FormPage())),
